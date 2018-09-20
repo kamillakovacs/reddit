a179116a42daa192685ab8cb8487e9297e8efccc
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-20 17:43:38 +0200

    feat(reddit): til downvote

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const PORT = 3000;
 const path = require('path');
 const mysql = require('mysql');
 // const cors = require('cors');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
-const conn = mysql.createConnection ({
+
+const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'HowNice',
@@ -32,13 +35,77 @@ app.get('/posts', (req, res) => {
         error: err.message,
       })
     } else {
-      res.status(200).json( {
-          posts,
+      res.status(200).json({
+        posts,
       })
     }
   })
 })
 
+app.post('/posts', jsonParser, (req, res) => {
+  let postTitle = req.body.title;
+  let postURL = req.body.url;
+
+  if (postTitle && postURL) {
+    conn.query(`INSERT INTO posts (title, url) values ('${postTitle}', '${postURL}');`, (err, result) => {
+      if (err) {
+        console.log(`Database error POST`);
+        res.status(500).send(err.message);
+        return;
+      } 
+      conn.query(`SELECT * from posts WHERE id = ${result.insertId};`, (err, specificPost) => {
+        if (err) {
+          console.log(`Database error SPECIFIC POST`)
+          res.status(500).send(err.message)
+          return;
+        } 
+        res.status(200).json({
+          specificPost,
+        })
+      })
+    } 
+  )}
+})
+
+app.put('/posts/:id/upvote', jsonParser, (req, res) => {
+  conn.query(`UPDATE posts SET score = score + 1 WHERE id = ${req.params.id};`, (err, result) => {
+    if (err) {
+      console.log(`Database error PUT UP`);
+      res.status(500).send(err.message);
+      return;
+    } 
+    conn.query(`SELECT * from posts WHERE id = ${req.params.id}`, (err2, specificPost) => {
+      if (err2) {
+        console.log(`Database error SPECIFIC PUT UP`)
+        res.status(500).send(err2.message)
+        return;
+      } 
+      res.status(200).json({
+        posts: specificPost,
+      })
+    })
+  })
+})
+
+app.put('/posts/:id/downvote', jsonParser, (req, res) => {
+  conn.query(`UPDATE posts SET score = score - 1 WHERE id = ${req.params.id};`, (err, result) => {
+    if (err) {
+      console.log(`Database error PUT DOWN`);
+      res.status(500).send(err.message);
+      return;
+    } 
+    conn.query(`SELECT * from posts WHERE id = ${req.params.id}`, (err2, specificPost) => {
+      if (err2) {
+        console.log(`Database error SPECIFIC PUT DOWN`)
+        res.status(500).send(err2.message)
+        return;
+      } 
+      res.status(200).json({
+        posts: specificPost,
+      })
+    })
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`App is up and running on port ${PORT}`);
