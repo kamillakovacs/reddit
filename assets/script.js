ea368f760343332b3db6683ec51d49960f575fe6
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-27 15:58:45 +0200

    feat(reddit and login): edits to reddit css and finished login

--- assets/script.js
+++ assets/script.js
@@ -12,29 +12,55 @@ window.onload = () => {
     console.log(postList)
  
     postList.forEach(post => {
-      let newDiv = document.createElement('div');
-      let postTitle = document.createElement('h3');
-      let upArrow = document.createElement('img')
-      let downArrow = document.createElement('img')
+      const newDiv = document.createElement('div');
+      const upArrow = document.createElement('img')
+      const downArrow = document.createElement('img')
+      const voteCounter = document.createElement('div')
       const postColumn = document.createElement('div')
-      const arrowColumn = document.createElement('div')
+      const arrowColumn = document.createElement('div');
+      const postLink = document.createElement('a');
+      const submission = document.createAttribute('div');
+      const editPosts = document.createElement('div');
+      const modifyDiv = document.createElement('div');
+      const removeDiv = document.createElement('div');
+      const modifyLink = document.createElement('a');
+      const removeLink = document.createElement('a');
 
       newDiv.classList.add('newPostDiv');
       upArrow.setAttribute('src', 'assets/upvote.png');
       downArrow.setAttribute('src', 'assets/downvote.png');
       upArrow.classList.add('uparrow');
       downArrow.classList.add('downarrow');
-      arrowColumn.classList.add('arrow-column')
-      postColumn.classList.add('post-column')
+      arrowColumn.classList.add('arrow-column');
+      postColumn.classList.add('post-column');
+      postLink.classList.add('post-link')
+      postLink.setAttribute('href', post.url);
+      postLink.setAttribute('target', "_blank");
+      editPosts.classList.add('edit-posts');
+      modifyLink.setAttribute('href', 'http://www.google.com');
+      removeLink.setAttribute('href', 'http://www.google.com');
+      
+      modifyDiv.classList.add('modify-div');
+      removeDiv.classList.add('remove-div');
 
       mainContent.appendChild(newDiv);
       newDiv.appendChild(arrowColumn)
       newDiv.appendChild(postColumn)
       arrowColumn.appendChild(upArrow)
+      arrowColumn.appendChild(voteCounter);
       arrowColumn.appendChild(downArrow)
-      postColumn.appendChild(postTitle);
+      postColumn.appendChild(postLink);
+      postColumn.appendChild(editPosts);
+      editPosts.appendChild(removeDiv);
+      removeDiv.appendChild(removeLink);
+      editPosts.appendChild(modifyDiv);
+      modifyDiv.appendChild(modifyLink);
 
-      postTitle.innerText = post.title;
+      postLink.innerText = post.title;
+      modifyLink.innerText = "modify";
+      removeLink.innerText = "remove";
+      voteCounter.innerHTML = post.score;
+
       });
     }
   }
